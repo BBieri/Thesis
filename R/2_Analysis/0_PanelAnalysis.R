a096d033ddb16d3b104a034d6e6dd1d30686fcd3
Bernhard Bieri <bernhard.bieri@graduateinstitute.ch>
2022-06-16 09:34:49 +0200

    Updated Panel data script

--- R/2_Analysis/0_PanelAnalysis.R
+++ R/2_Analysis/0_PanelAnalysis.R
@@ -36,80 +36,83 @@ oecd <- read.csv("data_raw/oecd.csv") %>%
 # Cooperation Network Measures
 ##############################
 
-# Strength FROW:
-load(paste0(data_path, "OECD_Panel_Strength_frow.rds"))
-Strength_frow <- saved
-rm(saved)
-# Eigenvector FROW:
-load(paste0(data_path, "OECD_Panel_Eigenvector_frow.rds"))
-Eigenvector_frow <- saved
-rm(saved)
+######
+# FDSM
+######
 
-frow_joined <- dplyr::inner_join(Strength_frow, Eigenvector_frow,
+# Strength FDSM:
+Strength_FDSM <- readRDS(paste0(data_path, "Centrality/OECD_Panel_Strength_fdsm.rds"))
+# Eigenvector FDSM:
+Eigenvector_FDSM <- readRDS(paste0(data_path, "Centrality/OECD_Panel_Eigenvector_fdsm.rds"))
+# Transitivity FDSM:
+Transitivity_FDSM <- readRDS(paste0(data_path, "Centrality/OECD_Panel_Transitivity_fdsm.rds")) %>%
+  dplyr::mutate(Year = as.character(Year))
+
+FDSM_joined <- dplyr::inner_join(Strength_FDSM, Eigenvector_FDSM,
                                  by = c("Country" = "Country", "Year" = "Year")) %>%
-  dplyr::mutate(COU = Country,
-                Year = as.character(Year)) %>%
-  dplyr::select(-Country)
+  dplyr::mutate(COU = Country) %>%
+  dplyr::select(-Country) %>%
+  dplyr::rename(Strength_FDSM = value.x,
+                Eigenvector_FDSM = value.y)
 
-# # Strength FDSM:
-# load(paste0(data_path, "OECD_Panel_Strength_fdsm.rds"))
-# Strength_frow <- saved
-# rm(saved)
-# # Eigenvector FDSM:
-# load(paste0(data_path, "OECD_Panel_Eigenvector_fdsm.rds"))
-# Eigenvector_frow <- saved
-# rm(saved)
-#
-# frow_joined <- dplyr::inner_join(Strength_frow, Eigenvector_frow,
-#                                  by = c("Country" = "Country", "Year" = "Year")) %>%
-#   dplyr::mutate(COU = countrycode::countrycode(Country, "iso3c", "iso2c"),
-#                 Year = as.numeric(Year))
+FDSM_joined <- dplyr::inner_join(FDSM_joined, Transitivity_FDSM,
+                                 by = c("COU" = "Country", "Year" = "Year")) %>%
+  dplyr::mutate(Year = as.character(Year)) %>%
+  dplyr::rename(Transitivity_FDSM = value) %>%
+  dplyr::relocate(COU, Year, Strength_FDSM, Eigenvector_FDSM, Transitivity_FDSM)
+
+######
+# SDSM
+######
 
 # Strength SDSM:
-load(paste0(data_path, "OECD_Panel_Strength_frow.rds"))
-Strength_frow <- saved %>%
-  dplyr::rename(Strength = value)
-rm(saved)
+Strength_SDSM <- readRDS(paste0(data_path, "Centrality/OECD_Panel_Strength_SDSM.rds"))
 # Eigenvector SDSM:
-load(paste0(data_path, "OECD_Panel_Eigenvector_frow.rds"))
-Eigenvector_frow <- saved %>%
-  dplyr::rename(Eigenvector = value)
-rm(saved)
+Eigenvector_SDSM <- readRDS(paste0(data_path, "Centrality/OECD_Panel_Eigenvector_SDSM.rds"))
+# Transitivity SDSM:
+Transitivity_SDSM <- readRDS(paste0(data_path, "Centrality/OECD_Panel_Transitivity_SDSM.rds")) %>%
+  dplyr::mutate(Year = as.character(Year))
 
-# Join Centrality measures
-
-frow_joined <- dplyr::inner_join(Strength_frow, Eigenvector_frow,
+SDSM_joined <- dplyr::inner_join(Strength_SDSM, Eigenvector_SDSM,
                                  by = c("Country" = "Country", "Year" = "Year")) %>%
-  dplyr::mutate(COU = countrycode::countrycode(Country, "iso3c", "iso2c"),
-                Year = as.numeric(Year))
+  dplyr::mutate(COU = Country) %>%
+  dplyr::select(-Country) %>%
+  dplyr::rename(Strength_SDSM = value.x,
+                Eigenvector_SDSM = value.y)
 
-# fdsm_joined <- dplyr::inner_join(Strength_fdsm, Eigenvector_fdsm,
-#                                  by = c("Country" = "Country", "Year" = "Year")) %>%
-#   dplyr::mutate(COU = countrycode::countrycode(Country, "iso3c", "iso2c"),
-#                 Year = as.numeric(Year))
+SDSM_joined <- dplyr::inner_join(SDSM_joined, Transitivity_SDSM,
+                                 by = c("COU" = "Country", "Year" = "Year")) %>%
+  dplyr::mutate(Year = as.character(Year)) %>%
+  dplyr::rename(Transitivity_SDSM = value) %>%
+  dplyr::relocate(COU, Year, Strength_SDSM, Eigenvector_SDSM, Transitivity_SDSM)
 
-sdsm_joined <- dplyr::inner_join(Strength_sdsm, Eigenvector_sdsm,
-                                 by = c("Country" = "Country", "Year" = "Year")) %>%
-  dplyr::mutate(COU = countrycode::countrycode(Country, "iso3c", "iso2c"),
-                Year = as.numeric(Year))
+# Join Centrality measures trhoughout methods
+
+Centrality_joined <- dplyr::inner_join(FDSM_joined, SDSM_joined,
+                                 by = c("COU" = "COU", "Year" = "Year")) %>%
+  dplyr::relocate(COU, Year, dplyr::ends_with("FDSM"), dplyr::ends_with("SDSM")) %>%
+  dplyr::mutate(Year = as.integer(Year)) %>%
+  dplyr::filter(Year <= 2015 & Year >= 1985 & COU %in% oecd$Code)
 
 ##############################
 # Innovation Measures
 ##############################
 
 # Business expenditures on aggregate R&D 2015 PPP USD
-berd <- readRDS(paste0(innovation_path, "BERD_PPP.rds")) %>%
+berd <- dplyr::tibble(readRDS(paste0(innovation_path, "BERD_PPP.rds"))) %>%
   dplyr::rename(berd = Value) %>%
   dplyr::select(Year, COU, berd) %>%
-  dplyr::mutate(Year = as.character(Year))
+  dplyr::filter(Year <= 2015 & Year >= 1985 & COU %in% oecd$Code) %>%
+  dplyr::mutate(Year = as.integer(Year))
 
 # Government expenditures on aggregate R&D 2015 PPP USD
-goverd <- readRDS(paste0(innovation_path, "GOVERD_controls.rds")) %>%
+goverd <- dplyr::tibble(readRDS(paste0(innovation_path, "GOVERD_controls.rds"))) %>%
   dplyr::select(-COU) %>%
   dplyr::rename(goverd = Value,
                 COU = iso3c) %>%
   dplyr::select(Year, COU, goverd) %>%
-  dplyr::mutate(Year = as.character(Year))
+  dplyr::filter(Year <= 2015 & Year >= 1985 & COU %in% oecd$Code) %>%
+  dplyr::mutate(Year = as.integer(Year))
 
 # Total Factor Productivity dataset 2015 PPP USD
 tfp <- readRDS(paste0(innovation_path, "tfp012_join.rds")) %>%
@@ -117,15 +120,16 @@ tfp <- readRDS(paste0(innovation_path, "tfp012_join.rds")) %>%
   dplyr::mutate(COU = countrycode::countrycode(COU, "iso2c", "iso3c",
                                                custom_match = c("EL" = "GRC"))) %>%
   dplyr::select(Year, COU, TFP0, TFP1, TFP2) %>%
-  dplyr::mutate(Year = as.character(Year))
+  dplyr::filter(Year <= 2015 & Year >= 1985 & COU %in% oecd$Code) %>%
+  dplyr::mutate(Year = as.integer(Year))
 
 # Environmental patents OECD dataset
 environmental_patents <- readRDS(paste0(innovation_path, "EnvPat.rds")) %>%
   dplyr::rename(EnvPat = Value,
                 Country = Inventor.country) %>%
   dplyr::select(Year, COU, EnvPat) %>%
-  dplyr::mutate(Year = as.character(Year)) %>%
-  dplyr::filter(COU %in% oecd$Code)
+  dplyr::filter(Year <= 2015 & Year >= 1985 & COU %in% oecd$Code) %>%
+  dplyr::mutate(Year = as.integer(Year))
 
 # Triadic patent counts
 triadic_patent <- readRDS(paste0(innovation_path, "tpf_inventor_country.rds")) %>%
@@ -133,7 +137,8 @@ triadic_patent <- readRDS(paste0(innovation_path, "tpf_inventor_country.rds")) %
   dplyr::rename(Year = First_Prio_Year,
                 triadic_patent = n) %>%
   dplyr::select(Year, COU, triadic_patent) %>%
-  dplyr::mutate(Year = as.character(Year))
+  dplyr::filter(Year <= 2015 & Year >= 1985 & COU %in% oecd$Code) %>%
+  dplyr::mutate(Year = as.integer(Year))
 
 # Innovation measures joined
 innovation <- purrr::reduce(list(berd, goverd, tfp, environmental_patents,
@@ -145,51 +150,38 @@ innovation <- purrr::reduce(list(berd, goverd, tfp, environmental_patents,
 ##############################
 
 # World Bank Controls
-WB <- readRDS(paste0(controls_path, "WB_controls.rds")) %>%
+WB <- dplyr::tibble(readRDS(paste0(controls_path, "WB_controls.rds"))) %>%
   dplyr::mutate(iso2c = countrycode::countrycode(iso2c,
                                                  "iso2c",
                                                  "iso3c")) %>%
   dplyr::rename(Year = year,
                 COU = iso2c) %>%
-  dplyr::mutate(Year = as.character(Year)) %>%
-  dplyr::select(-country)
+  dplyr::mutate(Year = as.integer(Year)) %>%
+  dplyr::select(-country) %>%
+  dplyr::filter(Year <= 2015 & Year >= 1985 & COU %in% oecd$Code)
 
 #############
 # EPS Measure
 #############
 
-EPS <- readRDS(paste0(eps_path, "eps.rds")) %>%
+EPS <- dplyr::tibble(readRDS(paste0(eps_path, "eps.rds"))) %>%
   dplyr::select(-COU) %>%
   dplyr::rename(EPS = Value,
                 COU = iso3c) %>%
   dplyr::select(Year, COU, EPS) %>%
-  dplyr::mutate(Year = as.character(Year))
+  dplyr::mutate(Year = as.integer(Year)) %>%
+  dplyr::filter(Year <= 2015 & Year >= 1985 & COU %in% oecd$Code)
 
 ########################
 # Everything else joined
 ########################
 
-independent_vars <- innovation %>%
+all_vars <- innovation %>%
+  dplyr::full_join(Centrality_joined, by = mergeCols) %>%
+  dplyr::full_join(EPS, by = mergeCols) %>%
   dplyr::full_join(WB, by = mergeCols) %>%
-  dplyr::full_join(EPS, by = mergeCols)
-
-############################ Join Different datasets ###########################
-
-
-# FROW
-panel_frow <- frow_joined %>%
-  dplyr::inner_join(independent_vars, by = mergeCols) %>%
-  dplyr::relocate(Year, COU)
-
-# FDSM
-# panel_fdsm <- fdsm_joined %>%
-#   dplyr::inner_join(innovation, by = mergeCols) %>%
-#   dplyr::relocate(Year, COU)
-
-# SDSM
-panel_sdsm <- sdsm_joined %>%
-  dplyr::inner_join(innovation, by = mergeCols) %>%
-  dplyr::relocate(Year, COU)
+  dplyr::select(-iso2c.x, -iso2c.y) %>%
+  dplyr::mutate(dplyr::across(where(is.numeric), ~na_if(., NaN)))
 
 #################### Subset Datasets to Considered Period ######################
 
@@ -212,60 +204,38 @@ fixest::setFixest_estimation(panel.id = ~COU + Year)
 # Increased Environmental Regulations are linked with increased rates of
 # innovation due to the factors below (or a combination of them).
 
-# Proxies:
+# Proxies for innovation:
 
 #   - Business expenditures on R&D: berd
 # POLS
-pols_berd_frow <- fixest::feols(fml = log(berd) ~ l(log(EPS), 5) +
-                             l(log(Strength), 5) + l(log(Eigenvector), 5) +
+pols_berd_fdsm <- fixest::feols(fml = log(berd) ~ l(log(EPS), 5) +
+                             l(log(Strength_FDSM), 5) + l(log(Transitivity_FDSM), 5) +
                              log(GDPCapita) +
                              log(ExportIntensity) +
                              log(ImportIntensity),
-                           data = panel_frow)
-
-pols_berd_frow <- plm::feols(fml = log(berd) ~ l(log(EPS), 5) +
-                                  l(log(Strength), 5) + l(log(Eigenvector), 5) +
-                                  log(GDPCapita) +
-                                  log(ExportIntensity) +
-                                  log(ImportIntensity),
-                                data = panel_frow)
-
-pols_berd_fdsm <- fixest::feols(fml = log(berd) ~ EPS +
-                                  l(log(Strength), 5) + l(log(Eigenvector), 5) +
-                                  log(GDPCapita) +
-                                  log(ExportIntensity) +
-                                  log(ImportIntensity),
-                                data = panel_fdsm)
+                           data = all_vars)
 
 pols_berd_sdsm <- fixest::feols(fml = log(berd) ~ l(log(EPS), 5) +
-                                  l(log(Strength), 5) + l(log(Eigenvector), 5) +
+                                  l(log(Strength_SDSM), 5) + l(log(Transitivity_SDSM), 5) +
                                   log(GDPCapita) +
                                   log(ExportIntensity) +
                                   log(ImportIntensity),
-                                data = panel_frow)
+                                data = all_vars)
 # Fixed effects: Country and Year
-fe_berd_frow <- fixest::feols(fml = log(berd) ~ l(log(EPS), 5) +
-                           l(log(Strength), 5) + l(log(Eigenvector), 5) +
-                           log(GDPCapita) +
-                           log(ExportIntensity) +
-                           log(ImportIntensity) | Year + COU,
-                         data = panel_frow,
-                         vcov = "NW")
-
 fe_berd_fdsm <- fixest::feols(fml = log(berd) ~ l(log(EPS), 5) +
-                           l(log(Strength), 5) + l(log(Eigenvector), 5) +
+                           l(log(Strength_FDSM), 5) + l(log(Transitivity_FDSM), 5) +
                            log(GDPCapita) +
                            log(ExportIntensity) +
                            log(ImportIntensity) | Year + COU,
-                         data = panel_frow,
+                         data = all_vars,
                          vcov = "NW")
 
 fe_berd_sdsm <- fixest::feols(fml = log(berd) ~ l(log(EPS), 5) +
-                        l(log(Strength), 5) + l(log(Eigenvector), 5) +
+                        l(log(Strength_SDSM), 5) + l(log(Transitivity_SDSM), 5) +
                         log(GDPCapita) +
                         log(ExportIntensity) +
                         log(ImportIntensity) | Year + COU,
-                      data = panel_frow,
+                      data = all_vars,
                       vcov = "NW")
 
 
